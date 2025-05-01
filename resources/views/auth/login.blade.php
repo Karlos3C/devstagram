f7e8e6a4ee22269e46d175e61dc2a0660763a7a0
Carlos Cabrera <carloscabreracarreon@gmail.com>
2025-05-01 02:28:19 -0600

    Update login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -58,7 +58,7 @@
                     </label>
                 </div>
 
-                <input type="submit" value="Iniciar Submit"
+                <input type="submit" value="Iniciar Sesión"
                     class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg" />
             </form>
         </div>
